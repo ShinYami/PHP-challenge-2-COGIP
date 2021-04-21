cbebe511feba6abef3d7abfff91c05213d8fc025
Conti Loris <loriisconti@gmail.com>
2021-04-21 14:33:15 +0200

    home last contact

--- views/app/home.php
+++ views/app/home.php
@@ -13,6 +13,24 @@ var_dump($params['last5People'][0])
             <table class="styled-table">
                 <thead>
                     <tr>
+                        <th>Numéro facture</th>
+                        <th>Dates</th>
+                        <th>Société</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+
+    </div>
+
+    <div class="table_content">
+        <h2 class="table_title">Derniers Contacts</h1>
+
+            <table class="styled-table">
+                <thead>
+                    <tr>
                         <th>Nom</th>
                         <th>Telephone</th>
                         <th>Email</th>
@@ -20,17 +38,35 @@ var_dump($params['last5People'][0])
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($params['peoples'] as $peoples) : ?>
-
+                    <?php foreach ($params['last5People'] as $last5people) : ?>
                         <tr>
-                            <td><a href="./listeContact/<?= $peoples['people_id']  ?>">
-                                    <?= $peoples['people_firstname'] ?> <?= $peoples['people_lastname'] ?>
-                                </a></td>
-                            <td><?= $peoples['people_phone'] ?></td>
-                            <td><?= $peoples['people_email'] ?></td>
-                            <td><?= $peoples['company_name'] ?></td>
+                            <td>
+                                <?= $last5people['people_firstname'] ?> <?= $last5people['people_lastname'] ?>
+                            </td>
+                            <td><?= $last5people['people_phone'] ?></td>
+                            <td><?= $last5people['people_email'] ?></td>
+                            <td><?= $last5people['company_name'] ?></td>
                         </tr>
                     <?php endforeach; ?>
+                </tbody>
+            </table>
+
+    </div>
+
+    <div class="table_content">
+        <h2 class="table_title">Dernières Sociétés</h1>
+
+            <table class="styled-table">
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>TVA</th>
+                        <th>Pays</th>
+                        <th>Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
                 </tbody>
             </table>
 
