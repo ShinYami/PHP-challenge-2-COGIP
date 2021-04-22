8b5c4adc7d4a5a0641e7706651a13369c8f78835
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-04-22 11:14:39 +0200

    company foreach

--- views/app/invoice/listInvoice.php
+++ views/app/invoice/listInvoice.php
@@ -1,5 +1,5 @@
 <h1>COGIP : Listing des factures</h1>
-
+<?php var_dump($params['invoices']) ?>
 <table class="styled-table">
     <thead>
         <tr>
@@ -11,12 +11,12 @@
     </thead>
     <tbody>
         <?php foreach ($params['invoices'] as $invoices) : ?>
-        <tr>
-            <td><a href="/invoices/<?= $invoices['invoice_id']  ?>"> <?= $invoices['invoice_number'] ?></a></td>
-            <td><?=$invoices['invoice_date']?></td>
-            <td><?=$invoices['company_name']?></td>
-            <td><?=$invoices['type_name']?></td>
-        </tr>
+            <tr>
+                <td><a href="/invoices/<?= $invoices['invoice_id']  ?>"> <?= $invoices['invoice_number'] ?></a></td>
+                <td><?= $invoices['invoice_date'] ?></td>
+                <td><?= $invoices['company_name'] ?></td>
+                <td><?= $invoices['type_name'] ?></td>
+            </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
