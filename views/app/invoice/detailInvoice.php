a0f57b39ee5a648a2992b58850797221cd1c13e8
Frederique Baillais <frederique.baillais@gmail.com>
2021-04-22 10:41:07 +0200

    fred

--- views/app/invoice/detailInvoice.php
+++ views/app/invoice/detailInvoice.php
@@ -1,15 +1,16 @@
+<?php var_dump($params['invoiceDetail']) ?>
 <?php var_dump($params['invoiceContact']) ?>
 <?php var_dump($params['invoiceCompany']) ?>
 
 
-<h1>Facture : <?= $params['invoices']['invoice_number'] ?></h1>
+<h1>Facture : <?= $params['invoiceDetail']['invoice_number'] ?></h1>
 
 <p>Société liée à la facture</p>
-<p>Nom : <?= $params['invoices']['company_name'] ?></p>
-<p>TVA : <?= $params['invoices']['company_tva'] ?></p>
-<p>Type de société : <?= $params['invoices']['type_name'] ?></p>
+<p>Nom : <?= $params['invoiceCompany']['company_name'] ?></p>
+<p>TVA : <?= $params['invoiceCompany']['company_tva'] ?></p>
+<p>Type de société : <?= $params['invoiceCompany']['type_name'] ?></p>
 
 <p>Personne de contact</p>
-<p>Nom : <?= $params['invoices']['people_firstname'] ?> <?= $params['invoices']['people_lastname'] ?></p>
-<p>Email : <?= $params['invoices']['people_email'] ?></p>
-<p>Phone : <?= $params['invoices']['people_phone'] ?></p>
+<p>Nom : <?= $params['invoiceContact']['people_firstname'] ?> <?= $params['invoiceContact']['people_lastname'] ?></p>
+<p>Email : <?= $params['invoiceContact']['people_email'] ?></p>
+<p>Phone : <?= $params['invoiceContact']['people_phone'] ?></p>
