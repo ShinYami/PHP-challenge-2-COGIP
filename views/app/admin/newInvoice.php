90a27f5c4049a99780c27e602d6bdb18c174fe0b
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-04-23 10:29:46 +0200

    d

--- views/app/admin/newInvoice.php
+++ views/app/admin/newInvoice.php
@@ -4,35 +4,42 @@ $invoice_number = '';
 $invoice_date = '';
 
 ?>
+<main class="content">
+    <form method="POST" action="/newInvoice">
+        <div class="group">
 
-<form method="POST" action="/newInvoice">
-    <div>
-        <label for="invoice_number">Numéro de facture</label>
-        <input type="text" name="invoice_number" value="<?php echo $invoice_number; ?>" required>
-        <small class='error'><?php echo "Numéro de facture requis"; ?></small>
-    </div>
-    <div>
-        <label for="invoice_date">Date de facture</label>
-        <input type="date" name="invoice_date" value="<?php echo $invoice_date; ?>" required>
-        <small class='error'><?php echo "Date de facture requise"; ?></small>
-    </div>
-    <div class="select">
-        <label for="company_id">Société</label>
-        <select name="company_id" id="select_company">
-            <?php foreach ($params['invoiceAllCompany'] as $company) : ?>
-                <option value="<?= $company['company_id'] ?>"> <?= $company['company_name'] ?></option>
-            <?php endforeach ?>
-        </select>
-        <small class='error'><?php echo "Société requise"; ?></small>
-    </div>
-    <div class="select">
-        <label for="people_id">Personne de contact pour la facture</label>
-        <select name="people_id" id="select_contact">
-            <?php foreach ($params['invoicePeople'] as $people) : ?>
-                <option value="<?= $people['people_id'] ?>"> <?= $people['people_firstname'] ?> <?= $people['people_lastname'] ?></option>
-            <?php endforeach ?>
-        </select>
-        <small class='error'><?php echo "Contact requis"; ?></small>
-    </div>
-    <button type="submit" name="button">Envoyer</button>
-</form>
+            <input type="text" name="invoice_number" value="<?php echo $invoice_number; ?>">
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label for="invoice_number">Numéro de facture</label>
+
+        </div>
+        <div class="group">
+
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <input type="date" name="invoice_date" value="<?php echo $invoice_date; ?>">
+            <label for="invoice_date">Date de facture</label>
+
+        </div>
+        <div class="select">
+            <select name="company_id" id="select_company">
+                <?php foreach ($params['invoiceAllCompany'] as $company) : ?>
+                    <option value="<?= $company['company_id'] ?>"> <?= $company['company_name'] ?></option>
+                <?php endforeach ?>
+            </select>
+        </div>
+        <div class="select">
+            <select name="people_id" id="select_contact">
+                <?php foreach ($params['invoicePeople'] as $people) : ?>
+                    <option value="<?= $people['people_id'] ?>"> <?= $people['people_firstname'] ?> <?= $people['people_lastname'] ?></option>
+                <?php endforeach ?>
+            </select>
+
+        </div>
+        <div class="group btn">
+            <button type="submit">Soumettre</button>
+        </div>
+
+    </form>
+</main>
