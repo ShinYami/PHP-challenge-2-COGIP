9c454ab7cdd34be6c70401cd71b2046b168bda49
Frederique Baillais <frederique.baillais@gmail.com>
2021-04-26 14:33:13 +0200

    dev to fred

--- views/app/home.php
+++ views/app/home.php
@@ -18,83 +18,83 @@
     <div class="table_content">
         <h2 class="table_title">Dernières Factures</h2>
 
-            <table class="styled-table">
-                <thead>
-                    <tr>
-                        <th>Numéro facture</th>
-                        <th>Dates</th>
-                        <th>Société</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
+        <table class="styled-table">
+            <thead>
+                <tr>
+                    <th>Numéro facture</th>
+                    <th>Dates</th>
+                    <th>Société</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
                 <?php foreach ($params['lastFiveInvoices'] as $lastFiveInvoices) : ?>
-                        <tr>
-                            <td><?= $lastFiveInvoices['invoice_number'] ?></td>
-                            <td><?= $lastFiveInvoices['invoice_date'] ?></td>
-                            <td><?= $lastFiveInvoices['company_name'] ?></td>
-                            <td><a class="delete_btn" href="">Supprimer</a></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    <tr>
+                        <td><?= $lastFiveInvoices['invoice_number'] ?></td>
+                        <td><?= $lastFiveInvoices['invoice_date'] ?></td>
+                        <td><?= $lastFiveInvoices['company_name'] ?></td>
+                        <td><a class="delete_btn" href="/deleteInvoice/<?= $lastFiveInvoices['invoice_id'] ?>">Supprimer</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
     </div>
 
     <div class="table_content">
         <h2 class="table_title">Derniers Contacts</h2>
 
-            <table class="styled-table">
-                <thead>
+        <table class="styled-table">
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Telephone</th>
+                    <th>Email</th>
+                    <th>Société</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($params['last5People'] as $last5people) : ?>
                     <tr>
-                        <th>Nom</th>
-                        <th>Telephone</th>
-                        <th>Email</th>
-                        <th>Société</th>
-                        <th></th>
+                        <td>
+                            <?= $last5people['people_firstname'] ?> <?= $last5people['people_lastname'] ?>
+                        </td>
+                        <td><?= $last5people['people_phone'] ?></td>
+                        <td><?= $last5people['people_email'] ?></td>
+                        <td><?= $last5people['company_name'] ?></td>
+                        <td><a class="delete_btn" href="/deleteContact/<?= $last5people['people_id'] ?>">Supprimer</a></td>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($params['last5People'] as $last5people) : ?>
-                        <tr>
-                            <td>
-                                <?= $last5people['people_firstname'] ?> <?= $last5people['people_lastname'] ?>
-                            </td>
-                            <td><?= $last5people['people_phone'] ?></td>
-                            <td><?= $last5people['people_email'] ?></td>
-                            <td><?= $last5people['company_name'] ?></td>
-                            <td><a class="delete_btn" href="">Supprimer</a></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
     </div>
 
     <div class="table_content">
         <h2 class="table_title">Dernières Sociétés</h2>
 
-            <table class="styled-table">
-                <thead>
-                    <tr>
-                        <th>Nom</th>
-                        <th>TVA</th>
-                        <th>Pays</th>
-                        <th>Type</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
+        <table class="styled-table">
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>TVA</th>
+                    <th>Pays</th>
+                    <th>Type</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
                 <?php foreach ($params['lastCompanies'] as $lastCompanies) : ?>
-                        <tr>
-                            <td><?= $lastCompanies['company_name'] ?></td>
-                            <td><?= $lastCompanies['company_tva'] ?></td>
-                            <td><?= $lastCompanies['company_country'] ?></td>
-                            <td><?= $lastCompanies['type_name'] ?></td>
-                            <td><a class="delete_btn" href="">Supprimer</a></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    <tr>
+                        <td><?= $lastCompanies['company_name'] ?></td>
+                        <td><?= $lastCompanies['company_tva'] ?></td>
+                        <td><?= $lastCompanies['company_country'] ?></td>
+                        <td><?= $lastCompanies['type_name'] ?></td>
+                        <td><a class="delete_btn" href="">Supprimer</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
     </div>
