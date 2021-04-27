870b124d55f024b339423c0b25e886d0030de431
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-04-27 11:01:23 +0200

    d

--- app/Models/Manager.php
+++ app/Models/Manager.php
@@ -9,18 +9,18 @@ abstract class Manager
     protected function dbConnect()
     {
 
-        //Get Heroku ClearDB connection information
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db = substr($cleardb_url["path"], 1);
-        $active_group = 'default';
-        $query_builder = TRUE;
-        // Connect to DB
-        $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+        // //Get Heroku ClearDB connection information
+        // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        // $cleardb_server = $cleardb_url["host"];
+        // $cleardb_username = $cleardb_url["user"];
+        // $cleardb_password = $cleardb_url["pass"];
+        // $cleardb_db = substr($cleardb_url["path"], 1);
+        // $active_group = 'default';
+        // $query_builder = TRUE;
+        // // Connect to DB
+        // $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-        // $db = new PDO('mysql:host=localhost;dbname=gocip;charset=utf8', 'root', 'root27', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET UTF8']);
-        return $conn;
+        $db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_8c6c10e133dac38;charset=utf8', 'b9943ffa0ab6b3', '383c672b', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET UTF8']);
+        return $db;
     }
 }
