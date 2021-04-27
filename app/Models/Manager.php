04b4865281c050bae90224103816d0b253810737
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-04-27 10:01:41 +0200

    p

--- app/Models/Manager.php
+++ app/Models/Manager.php
@@ -8,7 +8,19 @@ abstract class Manager
 {
     protected function dbConnect()
     {
-        $db = new PDO('mysql:host=localhost;dbname=gocip;charset=utf8', 'root', 'root27', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET UTF8']);
-        return $db;
+
+        //Get Heroku ClearDB connection information
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $cleardb_server = $cleardb_url["host"];
+        $cleardb_username = $cleardb_url["user"];
+        $cleardb_password = $cleardb_url["pass"];
+        $cleardb_db = substr($cleardb_url["path"], 1);
+        $active_group = 'default';
+        $query_builder = TRUE;
+        // Connect to DB
+        $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+        // $db = new PDO('mysql:host=localhost;dbname=gocip;charset=utf8', 'root', 'root27', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET UTF8']);
+        return $conn;
     }
 }
